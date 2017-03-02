d7000402f3b4a833010b4b600abd7857ef2b15c9
Ben Greaves <bsgreaves@gmail.com>
2017-03-02 00:22:22 -0600

    Assignment complete, ready for feedback

--- main.js
+++ main.js
@@ -28,10 +28,17 @@ var famousPeople = [{
 }];
 
 var cardHolder = document.getElementById("cardHolder");
+var userInput = document.getElementById("userInput")
+var cardCounter = 0;
 
-function createPerson(currentPerson) {
-    var newPerson = document.createElement("div");
-    newPerson.className = "card";
+var createPerson = currentPerson => { //prints the array to the DOM, called at bottom of page
+    var newPerson = document.createElement("person");
+    if (cardCounter % 2 === 0) { //wasn't sure how to implement cardCounter as a private variable
+        newPerson.className = "yellow-background card";
+    } else {
+        newPerson.className = "blue-background card";
+    }
+    cardCounter++;
     var newTitle = document.createElement("div");
     newTitle.className = "cardTitle";
     newTitle.innerText = currentPerson.title;
@@ -47,13 +54,32 @@ function createPerson(currentPerson) {
     var newLifespan = document.createElement("div");
     newLifespan.className = "cardLifespan";
     newLifespan.innerText = `${currentPerson.lifespan.birth} - ${currentPerson.lifespan.death}`
-    newPerson.appendChild(newName);
+    newPerson.appendChild(newName); //A lot of this could be cleaned up with chaining but I like it this way
     newPerson.appendChild(newTitle);
     newPerson.appendChild(newLifespan);
     newPerson.appendChild(newImg);
     newPerson.appendChild(newBio);
-
     cardHolder.appendChild(newPerson);
+    newPerson.addEventListener("click", personClicked)
+}
+
+var personClicked = e => {
+    let parentElement;
+    if (e.target.nodeName === "PERSON") {
+        parentElement = e.target;
+    } else {
+        parentElement = e.target.parentNode; //Makes sure I select the card!
+    }
+    parentElement.className += " dottedBorder"; //adds dotted border to person card
+    userInput.focus(); //adds focus to input field
+    userInput.addEventListener("keyup", function() { //pairs person's bio and input field
+        parentElement.children[4].innerText = userInput.value;
+    });
+    userInput.addEventListener("keypress", function(f) { //empties input field
+        if (f.keyCode === 13) {
+            userInput.value = "";
+        }
+    });
 }
 
 for (i = 0; i < famousPeople.length; i++) {
